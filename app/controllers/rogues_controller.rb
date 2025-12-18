216062898f53c45920e7ffbee86fca62ea6010e2
Shawn W. Devlin <swdevlin@gmail.com>
2025-12-18 14:50:13 -0500

    Rogues

---
+++ app/controllers/rogues_controller.rb
@@ -0,0 +1,16 @@
+class RoguesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :parsecs
   resources :solar_systems
   resources :subsectors
-  resources :sectors
+  resources :sectors do
+    resources :rogues, only: %i[index new create destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
