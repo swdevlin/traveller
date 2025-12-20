797db4784eb0d9c36a7e2f5c967abc280ced19eb
swdevlin <swdevlin@hey.com>
2025-12-20 14:48:18 -0500

    Parsecs can have 0 or more stellar objects, which are rogue objects not part of a solar system.

--- app/models/parsec.rb
+++ app/models/parsec.rb
@@ -2,6 +2,7 @@ class Parsec < ApplicationRecord
   validates :x, :y, :sector, presence: true
   belongs_to :sector
   has_many :solar_systems, dependent: :destroy
+  has_many :stellar_objects, dependent: :destroy
 
   validates :x, uniqueness: {
     scope: [:y, :sector_id],
