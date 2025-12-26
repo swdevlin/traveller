3f8bd250a2a0d851d9dcd15bac639628c65646ee
swdevlin <swdevlin@hey.com>
2025-12-26 16:09:20 -0500

    Marketing pages explaining what the site does.
    The default page is no longer the sectors list. It will be that when user accounts are implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get '/fairuse', to: 'marketing#fairuse', as: :fairuse
+
   # Defines the root path route ("/")
-  root 'sectors#index'
+  root 'marketing#index'
 end
