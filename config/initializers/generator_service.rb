8f1e592e0e833fd0ab7daad9eabc0a63a7901324
swdevlin <swdevlin@hey.com>
2026-01-03 09:20:50 -0500

    use 3007 for the generator service port

--- config/initializers/generator_service.rb
+++ config/initializers/generator_service.rb
@@ -1,1 +1,1 @@
-Rails.application.config.x.generator_service = ENV.fetch('GENERATOR_SERVICE', '')
+Rails.application.config.x.generator_service = ENV.fetch('GENERATOR_SERVICE_URL', 'http://generator-service:3007')
