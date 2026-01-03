ed14d7b08ebfb6b42be39d6e0030f62afd867d25
swdevlin <swdevlin@hey.com>
2026-01-03 11:14:23 -0500

    fix service name

--- config/initializers/generator_service.rb
+++ config/initializers/generator_service.rb
@@ -1,1 +1,1 @@
-Rails.application.config.x.generator_service = ENV.fetch('GENERATOR_SERVICE_URL', 'http://generator-service:3007')
+Rails.application.config.x.generator_service = ENV.fetch('GENERATOR_SERVICE_URL', 'http://generator:3007')
