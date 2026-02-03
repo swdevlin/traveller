9ef01c96382c7d57c71a0f618494ebe5c61f22a2
swdevlin <swdevlin@hey.com>
2026-02-03 18:33:36 -0500

    Add help link

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,5 +1,10 @@
 class HelpController < ApplicationController
   def index
+    help_dir = Rails.root.join('app', 'views', 'help')
+    @entries = Dir.glob(help_dir.join('*.html.erb'))
+                  .map { |f| File.basename(f, '.html.erb') }
+                  .reject { |name| name == 'index' || name.start_with?('_') }
+                  .sort
   end
 
   def build_specification
