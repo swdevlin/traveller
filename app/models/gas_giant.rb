4fa4801a164c1e877bfb8d39b878666623f8f732
swdevlin <swdevlin@hey.com>
2026-02-17 12:23:07 -0500

    Edit gas giant

--- app/models/gas_giant.rb
+++ app/models/gas_giant.rb
@@ -1,10 +1,26 @@
 class GasGiant < StellarObject
   include GeneratorMappings
 
+  before_validation :normalize_data_types
+
   generator_data_map(
     diameter: 'diameter',
     code: 'code',
     period: 'period',
     rotation: 'rotation'
   )
+
+  def self.permitted_params
+    [
+      :name, :notes, :orbit, :inclination, :eccentricity, :diameter, :mass,
+      data: [:code, :period, :rotation]
+    ]
+  end
+
+  private
+
+  def normalize_data_types
+    self.period = period.to_f if period.present?
+    self.rotation = rotation.to_f if rotation.present?
+  end
 end
