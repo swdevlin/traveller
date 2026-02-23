48309151bfde187bf2a384dad44bc8059a92b374
swdevlin <swdevlin@hey.com>
2026-02-23 18:28:39 -0500

    Start with MgT2 trade codes. Add option on trade code index page to import T5 trade codes.

---
+++ db/seed_data/mgt2_trade_codes.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+TRADE_CODES = [
+  { code: 'As', definition: 'Asteroid Belt' },
+  { code: 'De', definition: 'Desert' },
+  { code: 'Fl', definition: 'Fluid Oceans' },
+  { code: 'Ga', definition: 'Garden World' },
+  { code: 'Ic', definition: 'Ice-Capped' },
+  { code: 'Va', definition: 'Vacuum' },
+  { code: 'Wa', definition: 'Water World' },
+
+  { code: 'Ba', definition: 'Barren' },
+  { code: 'Lo', definition: 'Low Population' },
+  { code: 'Ni', definition: 'Non-Industrial' },
+  { code: 'Hi', definition: 'High Population' },
+
+  { code: 'Ag', definition: 'Agricultural' },
+  { code: 'Na', definition: 'Non-Agricultural' },
+  { code: 'In', definition: 'Industrial' },
+  { code: 'Po', definition: 'Poor' },
+  { code: 'Ri', definition: 'Rich' },
+
+  { code: 'Ht', definition: 'High Tech' },
+  { code: 'Lt', definition: 'Low Tech' }
+
+]
