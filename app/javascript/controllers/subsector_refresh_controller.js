67b2a0fe221ba50292e80fda954eb0fc2351b1e1
swdevlin <swdevlin@hey.com>
2026-02-28 16:12:04 -0500

    update map when generate job is finished

--- app/javascript/controllers/subsector_refresh_controller.js
+++ app/javascript/controllers/subsector_refresh_controller.js
@@ -1,12 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
-import { createConsumer } from "@rails/actioncable"
+import { Controller } from '@hotwired/stimulus'
+import { createConsumer } from '@rails/actioncable'
 
 export default class extends Controller {
   static values = { subsectorId: Number }
+  static targets = ['frame', 'map']
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
-      { channel: "SubsectorChannel", id: this.subsectorIdValue },
+      { channel: 'SubsectorChannel', id: this.subsectorIdValue },
       {
         received: (data) => this.handleMessage(data)
       }
@@ -20,16 +21,31 @@ export default class extends Controller {
   }
 
   handleMessage(data) {
-    if (data.event === "populating" || data.event === "finished") {
-      this.reload()
+    if (data.event === 'populating' || data.event === 'finished') {
+      this.#reloadFrame()
+    }
+    if (data.event === 'finished') {
+      this.#reloadMap()
     }
   }
 
-  reload() {
-    const src = this.element.getAttribute("src")
-    if (src) {
-      this.element.src = ""
-      this.element.src = src
-    }
+  #reloadFrame() {
+    this.frameTargets.forEach(frame => {
+      const src = frame.getAttribute('src')
+      if (src) {
+        frame.src = ''
+        frame.src = src
+      }
+    })
+  }
+
+  #reloadMap() {
+    this.mapTargets.forEach(obj => {
+      const url = obj.data
+      if (url) {
+        obj.data = ''
+        obj.data = url
+      }
+    })
   }
 }
