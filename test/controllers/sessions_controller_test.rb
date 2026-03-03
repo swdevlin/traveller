6ae31400dea6a7917e9d57eee930a9cf462faa64
swdevlin <swdevlin@hey.com>
2026-03-02 19:10:34 -0500

    Rubocop

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,28 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   setup { @user = User.take }
 
-  test "new" do
+  test 'new' do
     get new_session_path
     assert_response :success
   end
 
-  test "create with valid credentials" do
-    post session_path, params: { email_address: @user.email_address, password: "password" }
+  test 'create with valid credentials' do
+    post session_path, params: { email_address: @user.email_address, password: 'password' }
 
     assert_redirected_to root_path
     assert cookies[:session_id]
   end
 
-  test "create with invalid credentials" do
-    post session_path, params: { email_address: @user.email_address, password: "wrong" }
+  test 'create with invalid credentials' do
+    post session_path, params: { email_address: @user.email_address, password: 'wrong' }
 
     assert_redirected_to new_session_path
     assert_nil cookies[:session_id]
   end
 
-  test "destroy" do
+  test 'destroy' do
     sign_in_as(User.take)
 
     delete session_path
