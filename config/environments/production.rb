e9142ba7d212f7094708e0638dfc96c2fead8dff
swdevlin <swdevlin@hey.com>
2026-03-11 07:27:34 -0400

    update settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,13 @@ Rails.application.configure do
   # Specify outgoing SMTP server. Add smtp/* and app/* credentials via bin/rails credentials:edit.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV.fetch('SMTP_ADDRESS'),
+    address:              ENV['SMTP_ADDRESS'],
     port:                 ENV.fetch('SMTP_PORT', '587').to_i,
-    user_name:            ENV.fetch('SMTP_USER'),
-    password:             ENV.fetch('SMTP_PASSWORD'),
+    user_name:            ENV['SMTP_USER'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
-  }
+  }.compact
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
