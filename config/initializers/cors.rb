2206d64cc5b70b3f3bb1b1f0e2c9020a710ceca1
swdevlin <swdevlin@hey.com>
2026-03-12 08:52:54 -0400

    CORS for the API

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/api/*', headers: :any, methods: %i[get patch put options]
+    resource '/c/*/api/*', headers: :any, methods: %i[get patch put options]
   end
 end
