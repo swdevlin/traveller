d12ac59518d4e377a6fc8a854c6fb57a00b8ec78
swdevlin <swdevlin@hey.com>
2026-03-16 00:01:59 -0400

    App host for emails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,6 +4,8 @@
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  config.require_master_key = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # While tests run files are not watched, reloading is not necessary.
