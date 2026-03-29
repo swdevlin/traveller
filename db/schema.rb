bd08ef7599e6ddb6e487d0ec704522789221fbd8
swdevlin <swdevlin@hey.com>
2026-03-29 08:20:58 -0400

    Gem updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_21_123600) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_27_014341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "shared_extensions.pg_trgm"
