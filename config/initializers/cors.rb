9b1dbea92c5ff15a2cde4847af405cc52f68b37b
swdevlin <swdevlin@hey.com>
2026-04-20 17:48:24 -0400

    CORS update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/c/*/api/*', headers: :any, methods: %i[get patch put options]
+    resource '/c/*/api/*', headers: :any, methods: %i[get post patch put delete options]
   end
 end
