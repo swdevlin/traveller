9c10eb68b04643428a114e6c969e32a5e616a9b7
swdevlin <swdevlin@hey.com>
2026-05-20 07:14:07 -0400

    Not needed

--- app/javascript/controllers/colour_pair_controller.js
+++ app/javascript/controllers/colour_pair_controller.js
@@ -2,6 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['background', 'border']
+  #handler = null
 
   connect() {
     this.#handler = () => this.#sync()
