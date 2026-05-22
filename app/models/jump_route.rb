72d4d52dc15e7f4c678591bdebfe9ffddf757316
swdevlin <swdevlin@hey.com>
2026-05-22 11:22:56 -0400

    A few more line styles

--- app/models/jump_route.rb
+++ app/models/jump_route.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class JumpRoute < ApplicationRecord
-  LINE_STYLES = %w[solid dashed dotted].freeze
+  LINE_STYLES = %w[solid dashed dotted dash_dot dash_dot_dot long_dash short_dash].freeze
 
   validates :name,       presence: true
   validates :line_style, inclusion: { in: LINE_STYLES }
@@ -13,8 +13,12 @@ class JumpRoute < ApplicationRecord
 
   def stroke_dasharray
     case line_style
-    when 'dashed' then '16,8'
-    when 'dotted' then '3,5'
+    when 'dashed'       then '16,8'
+    when 'dotted'       then '3,5'
+    when 'dash_dot'     then '16,6,3,6'
+    when 'dash_dot_dot' then '16,6,3,6,3,6'
+    when 'long_dash'    then '28,10'
+    when 'short_dash'   then '8,6'
     end
   end
 end
