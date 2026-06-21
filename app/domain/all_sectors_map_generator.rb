cb5722b124b764435ebb7fd0514e41e45e62472c
swdevlin <swdevlin@hey.com>
2026-06-20 22:45:30 -0400

    rubocop

--- app/domain/all_sectors_map_generator.rb
+++ app/domain/all_sectors_map_generator.rb
@@ -139,7 +139,7 @@ class AllSectorsMapGenerator
         vips = vips.composite2(label, :over, x: x, y: y)
       rescue Vips::Error => e
         Rails.logger.error(
-          "AllSectorsMapGenerator composite2 failed " \
+          'AllSectorsMapGenerator composite2 failed ' \
           "vips_bands=#{vips.bands} vips_format=#{vips.format} vips_interp=#{vips.interpretation} " \
           "label_bands=#{label.bands} label_format=#{label.format} label_interp=#{label.interpretation} " \
           "error=#{e.message}"
